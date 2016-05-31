f5ce23c5c8fe039475565eb2f01316a032fb5ee6
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2016-05-31 17:01:53 +0100

    Change tests position.

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,13 +1,23 @@
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
-from django.test import TestCase, Client
+from django.test import SimpleTestCase, TestCase, Client
+from django.contrib import admin as default_admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 
 from admin_view_permission.admin import *
 
 from test_app.models import *
+
+
+
+class TestAdminViewPermissionAdminSite(SimpleTestCase):
+
+    def test_adminsite_class(self):
+        self.assertIsInstance(default_admin.site,
+                              AdminViewPermissionAdminSite)
+        self.assertIsInstance(default_admin.sites.site,
+                              AdminViewPermissionAdminSite)
 
 
 class TestModelAdminSuperUser(TestCase):
@@ -107,4 +117,54 @@ class TestModelAdminViewUser(TestCase):
                     args=(self.test10.pk,))
         )
         self.assertEqual(response.status_code, 403)
+
+
+class TestRegistration(SimpleTestCase):
+
+    def test_model_modeladmin_append(self):
+        self.assertIsInstance(default_admin.site._registry[TestModel1],
+                              AdminViewPermissionModelAdmin)
+        self.assertIsInstance(default_admin.site._registry[TestModel1],
+                              default_admin.ModelAdmin)
+
+    def test_other_model_modeladmin_append(self):
+        for model in default_admin.site._registry:
+            self.assertIsInstance(default_admin.site._registry[model],
+                                  AdminViewPermissionModelAdmin)
+            self.assertIsInstance(default_admin.site._registry[model],
+                                  default_admin.ModelAdmin)
+
+
+'''class TestAppsReady(SimpleTestCase):
+
+    def setUp(self):
+        self.appconfig = apps.get_app_config('admin_view_permission')
+        self.admin_site = AdminViewPermissionAdminSite()
+
+    @override_settings(ADMIN_VIEW_PERMISSION_MODELS=['test_app.TestModel1',])
+    def test_add_models_to_settings_option(self):
+        self.appconfig.ready()
+        self.admin_site.register(TestModel0)
+        self.admin_site.register(TestModel1)
+        self.admin_site.register(TestModel2)
+        self.admin_site.register(TestModel3)
+
+        for model in self.admin_site._registry:
+            if model._meta.label == 'test_app.TestModel1':
+                self.assertEqual(model._meta.permissions,
+                                 (('view_testmodel1', 'Can view testmodel1'), ))
+            else:
+                self.assertEqual(model._meta.permissions, ())
+
+    @override_settings(ADMIN_VIEW_PERMISSION_MODELS=[])
+    def test_empty_settings_option(self):
+        self.appconfig.ready()
+        self.admin_site.register(TestModel0)
+        self.admin_site.register(TestModel1)
+        self.admin_site.register(TestModel2)
+        self.admin_site.register(TestModel3)
+
+        for model in self.admin_site._registry:
+            self.assertEqual(model._meta.permissions, ())'''
+
 
