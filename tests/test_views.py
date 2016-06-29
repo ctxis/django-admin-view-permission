ab299ea6fa440f14cfed6bb3544d4a0d43b8c6e1
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2016-06-29 11:58:24 +0100

    Add post tests.

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 
 from .helpers import AdminViewPermissionViewsTestCase
 
+from .test_app.models import TestModel1
+
 
 class TestModelAdminViews(AdminViewPermissionViewsTestCase):
 
@@ -74,6 +76,30 @@ class TestModelAdminViews(AdminViewPermissionViewsTestCase):
 
         assert response.status_code == 200
 
+    def test_change_view_simple_user_unauthorized_post(self):
+        self.client.login(username='simple_user', password='simple_user')
+        data = {
+            'var1': 'test',
+            'var2': 'test',
+            'var3': 1,
+            'var4': self.object_testmodel0,
+            'testmodel2_set-TOTAL_FORMS': 0,
+            'testmodel2_set-INITIAL_FORMS': 0,
+            'testmodel3_set-TOTAL_FORMS': 0,
+            'testmodel3_set-INITIAL_FORMS': 0
+        }
+        response = self.client.post(
+            reverse('admin:%s_%s_change' % ('test_app', 'testmodel1'),
+                    args=(self.object_testmodel1.pk,)),
+            data
+        )
+        obj = TestModel1.objects.get(pk=self.object_testmodel1.pk)
+
+        assert response.status_code == 302
+        assert obj.var1 == 'Test' and obj.var1 != 'test'
+        assert obj.var2 == 'Test' and obj.var2 != 'test'
+        assert obj.var3 == 5 and obj.var3 != 1
+
     def test_change_view_super_user(self):
         self.client.login(username='super_user', password='super_user')
         response = self.client.get(
@@ -93,4 +119,25 @@ class TestModelAdminViews(AdminViewPermissionViewsTestCase):
         )
 
         assert response.status_code == 403
+
+    def test_delete_view_simple_user_unauthorized_post(self):
+        self.client.login(username='simple_user', password='simple_user')
+        response = self.client.post(
+            reverse('admin:%s_%s_delete' % ('test_app', 'testmodel1'),
+                    args=(self.object_testmodel1.pk,))
+        )
+
+        obj = TestModel1.objects.get(pk=self.object_testmodel1.pk)
+
+        assert response.status_code == 403
+        assert obj
+
+    def test_delete_view_super_user(self):
+        self.client.login(username='super_user', password='super_user')
+        response = self.client.get(
+            reverse('admin:%s_%s_delete' % ('test_app', 'testmodel1'),
+                    args=(self.object_testmodel1.pk,))
+        )
+
+        assert response.status_code == 200
 
