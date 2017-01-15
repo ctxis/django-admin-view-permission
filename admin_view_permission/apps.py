c082dfd928a27ca071c8f8b17caca4f08def2cf2
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2017-01-15 22:51:17 +0000

    Amend permissions instead of overriding them (Closes #8).

--- admin_view_permission/apps.py
+++ admin_view_permission/apps.py
@@ -17,6 +17,7 @@ def update_permissions(sender, app_config, verbosity, apps=global_apps, **kwargs
     # TODO: Maybe look at the registry not in all models
     for app in apps.get_app_configs():
         for model in app.get_models():
+            view_permission = 'view_%s' % model._meta.model_name
             if settings_models or (settings_models is not None and len(
                     settings_models) == 0):
 
@@ -26,14 +27,17 @@ def update_permissions(sender, app_config, verbosity, apps=global_apps, **kwargs
                 elif django_version() > DjangoVersion.DJANGO_18:
                     model_name = model._meta.label
 
-                if model_name in settings_models:
-                    model._meta.permissions = (
-                        ('view_%s' % model._meta.model_name,
+                if model_name in settings_models and view_permission not in \
+                        [perm[0] for perm in model._meta.permissions]:
+                    model._meta.permissions += (
+                        (view_permission,
                          'Can view %s' % model._meta.model_name),)
             else:
-                model._meta.permissions = (
-                    ('view_%s' % model._meta.model_name,
-                     'Can view %s' % model._meta.model_name),)
+                if view_permission not in [perm[0] for perm in
+                                            model._meta.permissions]:
+                    model._meta.permissions += (
+                        ('view_%s' % model._meta.model_name,
+                         'Can view %s' % model._meta.model_name),)
 
 
 class AdminViewPermissionConfig(AppConfig):
