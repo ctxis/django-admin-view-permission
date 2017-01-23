4e887032c55faa9670f1bbf442ee45d9f13b5b21
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2017-01-23 23:14:15 +0000

    Version 0.5.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-view-permission',
-    version='0.4',
+    version='0.5',
     packages=['admin_view_permission'],
     include_package_data=True,
     license='BSD License',
