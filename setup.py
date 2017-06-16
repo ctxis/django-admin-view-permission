af5816b71d0a75dbeadca8a5b13e999b92fec090
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2017-06-16 15:55:01 +0100

    Version 1.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-view-permission',
-    version='0.9',
+    version='1.0',
     packages=['admin_view_permission'],
     include_package_data=True,
     license='BSD License',
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
