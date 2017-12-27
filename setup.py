51480111a30a749f13ac194ca883f9845e68a017
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2017-12-27 11:57:28 +0000

    Fix flake8 errors on setup.py file.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-if sys.version_info > (2,7):
+if sys.version_info > (2, 7):
     install_requires = ['Django>=1.8']
 else:
     install_requires = ['Django>=1.8,<2.0']
