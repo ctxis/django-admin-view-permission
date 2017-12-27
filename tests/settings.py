e6b8b298665f6d052c94262cf0c75b12ef1569cb
Lefteris Nikoltsios <Lefteris.Nikoltsios@contextis.co.uk>
2017-12-27 10:25:49 +0000

    Fix tests for Django 2.0.

--- tests/settings.py
+++ tests/settings.py
@@ -41,13 +41,24 @@ INSTALLED_APPS = [
     'tests.test_app'
 ]
 
+# For version of Django smaller than 1.10
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# For versions of Django bigger or equal than 1.10
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
